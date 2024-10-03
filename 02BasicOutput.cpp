c674cd2c6a6c4e490e52597d7d3b39d8048f4e30
Kien Nava <Kiennava@gmail.com>
2024-10-03 08:32:22 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "OUR LADY OF LOURDES COLLEGES FOUNDATION" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT 1-B" << endl;
+	cout << "MHYCO ALLEN KIEN NAVA" << endl;
+	cout << "P-7 Barangay Lag-on Daet, Camarines Norte";
+	return 0;
+}
