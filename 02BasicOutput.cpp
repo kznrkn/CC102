4d087d3bf1e30368f7437bddd6c0b5cff612430e
Kien Nava <Kiennava@gmail.com>
2024-10-03 10:50:18 +0800

    Update 02BasicOutput.cpp

--- 02BasicOutput.cpp
+++ 02BasicOutput.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 int main() {
-	
 	cout << "OUR LADY OF LOURDES COLLEGES FOUNDATION" << endl;
 	cout << "College of Computer Studies" << endl;
 	cout << "BSIT 1-B" << endl;
 	cout << "MHYCO ALLEN KIEN NAVA" << endl;
-	cout << "P-7 Barangay Lag-on Daet, Camarines Norte";
+	cout << "P7 Barangay Lag-on Daet, Cam Norte";
 	return 0;
 }
