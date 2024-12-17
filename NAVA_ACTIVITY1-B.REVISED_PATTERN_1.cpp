6edcb62535c392d46fb20ae87ced9c933c4ff6de
Kien Nava <Kiennava@gmail.com>
2024-12-17 15:16:55 +0800

    Update NAVA_ACTIVITY1-B.REVISED_PATTERN_1.cpp

--- NAVA_ACTIVITY1-B.REVISED_PATTERN_1.cpp
+++ NAVA_ACTIVITY1-B.REVISED_PATTERN_1.cpp
@@ -2,17 +2,14 @@
 using namespace std;
 int main() {
 	
-	
 	for(int i=7; i>=1; i--){
-	for(int j=7-i; j>0; j--)
-		
-	cout << "  ";
-	
+	for(int j=7-i; j>0; j--)	
+	 cout << "  ";
 	for(int k=i; k>0; k--) {
 	if (k % 2 ==0) {
-		cout << "@ ";
+         cout << "@ ";
 	} else {
-		cout << "# ";
+	 cout << "# ";
 	}
 	}
 	cout << endl;
